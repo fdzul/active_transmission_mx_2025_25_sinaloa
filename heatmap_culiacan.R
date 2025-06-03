975f8a8a260cff89d826adab4736e9b0dca2ed8a
fdzul <felipe.dzul.m@gmail.com>
2025-06-03 16:05:34 -0600

    This commit update the dash

--- heatmap_culiacan.R
+++ heatmap_culiacan.R
@@ -2,22 +2,11 @@
 # Step 1. load the dengue geocoded dataset ####
 load("~/OneDrive/proyects/geocoding_mex/2025/8.RData/denmex_2025.RData")
 
-y <- z |>
-    dplyr::mutate(x = long,
-                  y = lat) |>
-    sf::st_as_sf(coords = c("x", "y"),
-                 crs = 4326) |>
+z <- z |>
     dplyr::filter(accuracy != "locality")
-table(y$accuracy)
+    
 
-cul <- rgeomex::extract_locality(cve_edo = "25",
-                                      locality = "Culiacan de Rosales")
-
-y <- y[cul, ]
-
-mapview::mapview(y)
-
-densnv::mp_heatmap(geocoded_datasets = y,
+densnv::mp_heatmap(geocoded_datasets = z,
                    cve_edo = "25",
                    locality = "Culiacan de Rosales",
                    status_caso = c(1, 2),
